044468a7b27181c00b0ce1aa906a48a7614d01e6
byak84 <fffc@inbox.ru>
2020-09-15 01:10:20 +0700

    com1

--- src/App.vue
+++ src/App.vue
@@ -53,9 +53,14 @@ export default {
       fetch(this.apiURL + "add/" + this.currentCategory, {
         method: 'POST',
         headers: {
+          'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(post_options)
+      }).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
       })
     },
 
