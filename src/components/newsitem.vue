ba62cc72dc8cb3f76c9e27062455ee856874ac59
byak84 <fffc@inbox.ru>
2020-09-23 01:26:16 +0700

    drag and drop

--- src/components/newsitem.vue
+++ src/components/newsitem.vue
@@ -1,15 +1,47 @@
 <template>
-  <li class="item" @click="itemclick" v-bind:data-category="category" v-bind:data-id="news.id">{{ news.name }}</li>
+  <li class="item" @click="itemclick" draggable="true"
+      v-bind:data-category="category"
+      v-bind:data-id="news.id"
+      v-on:dragstart="dragStart"
+      v-on:dragover="dragOver"
+      v-on:dragenter="dragEnter"
+      v-on:dragleave="dragLeave"
+      v-on:drop="dragDrop"
+      v-bind:class="{overed: isOvered}">{{ news.name }}
+  </li>
 </template>
 
 <script>
 export default {
   props: ['news', 'category'],
+  data: () => {
+    return {
+      isOvered: false,
+    }
+  },
   methods: {
     itemclick: function (event) {
       const path = event.target.dataset.category + "/" + event.target.dataset.id;
       this.$emit('itemclick', path);
+    },
+    dragStart:function (evn) {
+      console.log(this.news);
+    },
+    dragOver: function (evn) {
+      evn.preventDefault();
+    },
+    dragEnter: function () {
+      this.isOvered = true;
+    },
+    dragLeave: function () {
+      this.isOvered = false;
+    },
+    dragDrop: function (evt) {
+      console.log(this.news);
+      this.isOvered = false;
     }
+
+
   }
 }
 </script>
@@ -25,6 +57,10 @@ export default {
 
 .item:hover {
   text-decoration: underline;
+}
+
+.overed {
+  background-color: cornflowerblue;
 }
 
 </style>
