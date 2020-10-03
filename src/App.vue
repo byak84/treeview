2ffdfd150871f3acd984608ca538414e407b5fed
byak84 <fffc@inbox.ru>
2020-10-04 01:38:51 +0700

    initial 2

--- src/App.vue
+++ src/App.vue
@@ -3,109 +3,75 @@
     <action v-on:add_news="add_news"
     />
 
-    <treeview v-bind:tree="this['myTree']"
+    <treeview v-bind:tree="treeJSON"
               v-on:itemclick="itemclick"
               v-on:deleteItem="deleteItem"
     />
 
-    <textview v-bind:text="this['text']"/>
+
   </div>
 </template>
 
 <script>
 import Treeview from "@/components/treeview"
-import Textview from "@/components/textview"
 import Action from "@/components/action"
-import category from "@/components/category";
-
+import json from './menu.json'
 export default {
   name: 'App',
   components: {
-    Treeview, Textview, Action,
+    Treeview, Action,
   },
 
   data: () => {
     return ({
-      apiURL: "http://192.168.2.65:5000",
-      myTree: {},
-      text: "",
-      currentCategory: "",
+      treeJSON: {},
     });
   },
   methods: {
     itemclick: function (path) {
-      this.currentCategory = path.substr(1).split("/", 1).toString();
-
-      fetch(this.apiURL + path)
-          .then(response => {
-            response.json()
-                .then(data => {
-                  this.text = data.text;
-                })
-          })
-          .catch(err => {
-            alert(err);
-          })
+      // this.currentCategory = path.substr(1).split("/", 1).toString();
+      // fetch(this.apiURL + path)
+      //     .then(response => {
+      //       response.json()
+      //           .then(data => {
+      //             this.text = data.text;
+      //           })
+      //     })
+      //     .catch(err => {
+      //       alert(err);
+      //     })
     },
     add_news: function (link) {
-      console.log(link);
-      let post_options = {
-        link: link
-      }
-      fetch(this.apiURL + "/add/" + this.currentCategory + "/", {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(post_options)
-      }).then(res => {
-        // console.log(res)
-      }).catch(err => {
-        // console.log(err)
-      })
+      // console.log(link);
+      // let post_options = {
+      //   link: link
+      // }
+      // fetch(this.apiURL + "/add/" + this.currentCategory + "/", {
+      //   method: 'POST',
+      //   headers: {
+      //     'Accept': 'application/json',
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(post_options)
+      // }).then(res => {
+      //   // console.log(res)
+      // }).catch(err => {
+      //   // console.log(err)
+      // })
     },
     deleteItem: function (uuid, cat) {
-      console.log("deleteItem: ", uuid);
-
-      this.myTree[cat] = this.myTree[cat]
-          .filter(t => {
-            return t.uuid != uuid;
-          })
-      // if (this.myTree[cat].length === 0) {
-      //   this.myTree[cat].push({
-      //     "name": "Категория пуста",
-      //     "order": 0,
-      //     "text": "",
-      //     "uuid": "0"
-      //   });
-      // }
-      // console.log(this.myTree);
+      // console.log("deleteItem: ", uuid);
+      //
+      // this.myTree[cat] = this.myTree[cat]
+      //     .filter(t => {
+      //       return t.uuid != uuid;
+      //     })
     },
     loadTree: function () {
-      fetch(this.apiURL)
-          .then(response => {
-            response.json()
-                .then(data => {
-                  this.myTree = data;
-                })
-          })
-          .catch(err => {
-            alert(err);
-          })
+      this.treeJSON = json;
     },
-    // addEmpty:
 
   },
-  watch: {
-    // myTree: function (tree) {
-    //   console.log("Сработка watch", tree);
-    //   for (let category in tree) {
-    //     console.log(category);
-    //   }
-    // }
-  },
-  computed: {},
   created() {
     this.loadTree();
   }
--- src/components/treeview.vue
+++ src/components/treeview.vue
@@ -1,14 +1,6 @@
 <template>
 <div class="treeview">
-  <ul class="root_ul">
-    <category v-for="(newsArray,cat) in tree"
-              :key="newsArray.uuid"
-              v-bind:title="cat"
-              v-bind:newsArray="newsArray"
-              v-on:itemclick="itemclick"
-              v-on:deleteItem="deleteItem"
-    />
-  </ul>
+
 </div>
 </template>
 
@@ -19,16 +11,7 @@ export default {
   components: {Category},
   props: ['tree'],
   methods: {
-    itemclick: function (path, contex) {
-      this.$emit("itemclick", path);
-      let children = this.$children;
-      children.forEach(function (cat, i, children) {
-        cat.unselectAll(contex);
-      })
-    },
-    deleteItem: function (uuid, cat) {
-      this.$emit("deleteItem", uuid, cat)
-    }
+
   }
 }
 </script>
@@ -39,14 +22,9 @@ export default {
   padding: 0px;
   width: 30%;
   height: 100%;
-  /*background-color: #7fffd4;*/
+  background-color: #aaaaaa;
   float: left;
 }
 
-.root_ul {
-  margin: 0;
-  padding: 0;
-  list-style-type: none;
-}
 
 </style>
